8a5964807028afd23e30a4642ff894266bbe80fd
arlekin1208 <arlekin12089@gmail.com>
2022-02-15 11:45:35 +0100

    fix header

--- js/components/MainMenu.js
+++ js/components/MainMenu.js
@@ -1,5 +1,20 @@
 function MainMenu() {
-  console.log("menu");
+  const menu = document.querySelector(".main-menu--mobile");
+  const menuToggle = document.querySelector(".menu-toggle");
+
+  window.addEventListener("load", () => {
+    toggleMobileMenu();
+  });
+
+  function toggleMobileMenu() {
+    if (menuToggle) {
+      menuToggle.addEventListener("click", (event) => {
+        event.preventDefault();
+        menuToggle.classList.toggle("menu-open");
+        menu.classList.toggle("open");
+      });
+    }
+  }
 }
 
 export default MainMenu();
