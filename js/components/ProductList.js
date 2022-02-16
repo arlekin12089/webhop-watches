c486590980f8a9fe570afa2d1575486c0043d7d1
Arlekin1208 <arlekin12089@gmail.com>
2022-02-16 19:55:38 +0100

    add menu links query strings, add show items for proper category

--- js/components/ProductList.js
+++ js/components/ProductList.js
@@ -1,5 +1,70 @@
 function ProductList() {
-  console.log("data");
+  const queryString = new URLSearchParams(location.search);
+  const qsCategory = queryString.get("category");
+  const categoryName = document.getElementById("category-name");
+  const productsWrap = document.getElementById("products-list");
+  const categoryMenuItem = document.getElementById("categoryMenuItem");
+
+  function listProducts(listProducts) {
+    listProducts.forEach((item) => {
+      let elem = document.createElement("article");
+      elem.classList.add("product");
+      elem.innerHTML = `       
+              <div class="product__photo">
+                <img
+                  src="sass/${item.image}"
+                  alt="${item.image}"
+                />
+                <div class="product__icon">
+                  <a href="#" target="_blank" class="product__link">
+                    <i class="fa fa-search"></i>
+                  </a>
+                  <button class="addToCard product__link">
+                    <i class="fa fa-shopping-cart"></i>
+                  </button>
+                </div>
+              </div>
+              <footer class="product__description center">
+                <h4 class="name">${item.name}</h4>
+                <p class="price">${item.price}</p>
+              </footer>
+        `;
+      productsWrap.appendChild(elem);
+    });
+  }
+
+  function changeTitle(title) {
+    categoryName.innerHTML = title;
+  }
+
+  async function getProducts() {
+    if (qsCategory === "all") {
+      changeTitle("Alla klockor");
+      categoryMenuItem.innerHTML = "Alla klockor";
+    } else {
+      const categoriesResponse = await fetch("js/Categories.json");
+      const categories = await categoriesResponse.json();
+      let findCategory = categories.find(
+        (category) => qsCategory === category.id
+      );
+      changeTitle(findCategory.name);
+      categoryMenuItem.innerHTML = findCategory.name;
+    }
+    const productsResponse = await fetch("js/Products.json");
+    const products = await productsResponse.json();
+
+    if (qsCategory === "all") {
+      listProducts([...products]);
+    } else {
+      let filteredProducts = products.filter(
+        (item) => item.categoryId === qsCategory
+      );
+      listProducts([...filteredProducts]);
+    }
+  }
+
+  getProducts();
 }
+
 
 export default ProductList();
--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
 import Data from "./Data.js";
 import MainMenu from "./components/MainMenu.js";
 import ProductList from "./components/ProductList.js";
-
