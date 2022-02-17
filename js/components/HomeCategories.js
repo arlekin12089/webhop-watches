7c50ef5d28bde11ac0ff0a01dad93c90c66bf399
VendelaAJ <vendela.swenson@hotmail.se>
2022-02-17 13:31:04 +0100

    fixed footer position

--- js/components/HomeCategories.js
+++ js/components/HomeCategories.js
@@ -2,12 +2,14 @@ import * as ProductRepository from "./ProductRepository.js";
 
 const homeCategories = document.getElementById("categoriesHome");
 
+console.log(homeCategories);
+
 function listCategoriesHome(list) {
   list.forEach((item) => {
     let elem = document.createElement("div");
     elem.classList.add("category-item");
     elem.innerHTML = `
-      		<img src="sass/${item.image}" alt="">
+      		<img src="sass/${item.image}" alt="bakgrund klocka">
   				<h2 class="title">${item.name}</h2>
   				<p class="desc">${item.description}</p>
       `;
